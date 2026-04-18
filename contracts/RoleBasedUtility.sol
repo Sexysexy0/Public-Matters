3ac0a1ae5425439bb8b9d17c0013ad80b3bc125f
donnnq <gemervin14@gmail.com>
2026-04-18 13:59:01 +0000

    DEPLOY: Couple Co-op - Underwater Lazarus Edition

---
+++ contracts/RoleBasedUtility.sol
@@ -0,0 +1,10 @@
+// RoleBasedUtility.sol
+pragma solidity ^0.8.0;
+
+contract RoleBasedUtility {
+    // Nagbibigay ng unique perks sa mag-jowa
+    function assignRoles(address _p1, address _p2) public {
+        // Player 1: Combat & Exploration
+        // Player 2: Crafting & Ship Management
+    }
+}
