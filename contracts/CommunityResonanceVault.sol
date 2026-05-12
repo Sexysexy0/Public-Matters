a8c19c1bb3368dbcfed8332056ae592b941e2542
donnnq <gemervin14@gmail.com>
2026-05-12 13:40:51 +0000

    Drop: CommunityResonanceVault – safeguard fan resonance, preservation equity, and systemic foresight

--- contracts/CommunityResonanceVault.sol
+++ contracts/CommunityResonanceVault.sol
@@ -2,35 +2,36 @@
 pragma solidity ^0.8.20;
 
 contract CommunityResonanceVault {
-    struct Suggestion {
-        address participant;
-        string idea;
+    struct Request {
+        address fan;
+        string title;
+        string context;
         uint256 timestamp;
         bool safeguarded;
     }
 
-    Suggestion[] public suggestions;
+    Request[] public requests;
 
-    event SuggestionLogged(address indexed participant, string idea);
-    event SuggestionSafeguarded(uint256 indexed id, address verifier);
+    event RequestLogged(address indexed fan, string title, string context);
+    event RequestSafeguarded(uint256 indexed id, address verifier);
 
-    function logSuggestion(string memory _idea) public {
-        suggestions.push(Suggestion(msg.sender, _idea, block.timestamp, false));
-        emit SuggestionLogged(msg.sender, _idea);
+    function logRequest(string memory _title, string memory _context) public {
+        requests.push(Request(msg.sender, _title, _context, block.timestamp, false));
+        emit RequestLogged(msg.sender, _title, _context);
     }
 
-    function safeguardSuggestion(uint256 _id) public {
-        require(_id < suggestions.length, "Invalid ID");
-        suggestions[_id].safeguarded = true;
-        emit SuggestionSafeguarded(_id, msg.sender);
+    function safeguardRequest(uint256 _id) public {
+        require(_id < requests.length, "Invalid ID");
+        requests[_id].safeguarded = true;
+        emit RequestSafeguarded(_id, msg.sender);
     }
 
-    function getSuggestion(uint256 _id) public view returns (Suggestion memory) {
-        require(_id < suggestions.length, "Invalid ID");
-        return suggestions[_id];
+    function getRequest(uint256 _id) public view returns (Request memory) {
+        require(_id < requests.length, "Invalid ID");
+        return requests[_id];
     }
 
-    function totalSuggestions() public view returns (uint256) {
-        return suggestions.length;
+    function totalRequests() public view returns (uint256) {
+        return requests.length;
     }
 }
