50ae8b72bcf811ca8a7c49d68a3087b6498efe63
donnnq <gemervin14@gmail.com>
2026-05-12 14:10:52 +0000

    Drop: ResonanceEquityOracle – safeguard resonance equity, authenticity fairness, and systemic dignity

--- contracts/ResonanceEquityOracle.sol
+++ contracts/ResonanceEquityOracle.sol
@@ -2,31 +2,31 @@
 pragma solidity ^0.8.20;
 
 contract ResonanceEquityOracle {
-    struct Resonance {
-        address participant;
-        string context;
+    struct ResonanceEquity {
+        address overseer;
+        string arc;
         string safeguard;
         uint256 timestamp;
         bool safeguarded;
     }
 
-    Resonance[] public resonances;
+    ResonanceEquity[] public resonances;
 
-    event ResonanceLogged(address indexed participant, string context, string safeguard);
-    event ResonanceSafeguarded(uint256 indexed id, address verifier);
+    event ResonanceEquityLogged(address indexed overseer, string arc, string safeguard);
+    event ResonanceEquitySafeguarded(uint256 indexed id, address verifier);
 
-    function logResonance(string memory _context, string memory _safeguard) public {
-        resonances.push(Resonance(msg.sender, _context, _safeguard, block.timestamp, false));
-        emit ResonanceLogged(msg.sender, _context, _safeguard);
+    function logResonanceEquity(string memory _arc, string memory _safeguard) public {
+        resonances.push(ResonanceEquity(msg.sender, _arc, _safeguard, block.timestamp, false));
+        emit ResonanceEquityLogged(msg.sender, _arc, _safeguard);
     }
 
-    function safeguardResonance(uint256 _id) public {
+    function safeguardResonanceEquity(uint256 _id) public {
         require(_id < resonances.length, "Invalid ID");
         resonances[_id].safeguarded = true;
-        emit ResonanceSafeguarded(_id, msg.sender);
+        emit ResonanceEquitySafeguarded(_id, msg.sender);
     }
 
-    function getResonance(uint256 _id) public view returns (Resonance memory) {
+    function getResonanceEquity(uint256 _id) public view returns (ResonanceEquity memory) {
         require(_id < resonances.length, "Invalid ID");
         return resonances[_id];
     }
